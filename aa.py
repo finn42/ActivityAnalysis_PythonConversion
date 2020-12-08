5346f1f2fd05b50c98d206be1389ddc6e85f8e9d
Finn Upham <finn.upham@gmail.com>
2020-12-08 14:56:40 -0500

    Added simple coodination test

--- aa.py
+++ aa.py
@@ -3,6 +3,7 @@
 import pandas as pd
 import numpy as np
 from scipy.interpolate import interp1d
+from scipy.stats import binom, poisson, chisquare
 
 def activityCount(Data,FrameSize,HopSize,Thresh,actType):
     ''' def activityCount(Data,FrameSize,HopSize,Thresh,actType)
@@ -62,4 +63,87 @@ def activityCount(Data,FrameSize,HopSize,Thresh,actType):
     Acts['Total'] = Acts.sum(1)/len(cols) # ratio of signals active per frame
     return Acts
 
+def equisplit(counts,nBins):
+    ''' function to split discrete distribution into maximally-even sized nBins.
+    If the distribution does not split cleanly (bin with less than 5 samples), 
+    repeat evaluation with one less bin, to a min of 3 bins.
+    
+     Input: Counts - a panda series of counts (model), indexed sequentially. 
+     Output: Index lists of cuts for bins 
+     
+     Function used by simpleActivityTest
+ 	'''
+    minim = 5
+    n = nBins
+
+    cumV = counts.cumsum()
+    eTot = cumV.iloc[-1]
+    
+    cuts = [counts.index[0]]
+    for i in range(1,n):
+        crit = (cumV-eTot*(i/n))
+        crit[crit<0] = eTot
+        #crit = abs((cumV-cumV.iloc[-1]*(i/n)))
+        cuts.append(crit.idxmin())
+    cuts.append(counts.index[-1])
+    
+    dists = []
+    for i in range(len(cuts)-1):
+        dists.append(counts.loc[cuts[i]:cuts[i+1]-1].sum())
+
+    if np.min(dists)<5:
+        print('Too many bins')
+        if nBins>2:
+            cuts = equisplit(counts,nBins-1)
+        else:
+            print('Not enough samples to cut for GoF test.')
+    return cuts
+
+def simpleActivityTest(A,Np,Nbins):
+    ''' function to run a goodness of fit test on a random activity model
+    against the measure distrubution of activity levels
+
+    stest = {'Chi2':st,'pvalue':p,'Counts':Counts,'Bins':dists}
+    '''
+    ac = np.round(A.values*Np) # get these back to count values, integers
+    meas = pd.Series(ac).value_counts()
+    L=len(ac)
+    aL = np.arange(Np)
+    Counts = pd.DataFrame(index = aL)
+    Counts['Measured'] = 0
+    Counts.loc[meas.index,'Measured'] = meas.values
+    if Np <100:
+        Counts['Model'] = binom.pmf(aL, Np, A.sum()/L)*L
+    else:
+        Counts['Model'] = poisson.pmf(aL, Np*A.sum()/L)*L
+
+    cuts = equisplit(Counts['Model'],Nbins)    
+    dists = pd.DataFrame()
+    for i in range(len(cuts)-1):
+        dists = dists.append(Counts.loc[cuts[i]:cuts[i+1]-1,:].sum(), ignore_index=True)
+
+    st,p = chisquare(dists['Measured'], f_exp=dists['Model'])
+
+    stest = {'Chi2':st,'pvalue':p,'Counts':Counts,'Bins':dists}
+    
+    return stest
+
+
+def score_C(pvals):
+    # pval must be an np.array
+    C =  -np.log10(pvals+10.0**(-16)).mean() 
+    return C
+
+def coordScoreSimple(Data,FrameSize,Thresh,actType,Nbins):
+    t = pd.Series(Data.index)
+    sF = 1/t.diff().median()
+    winSize = int(FrameSize/sF)
+    N = Data.shape
+    CS=[]
+    for i in range(winSize):
+        Acts = activityCount(Data.loc[i:],FrameSize,FrameSize,Thresh,actType)
+        stest = simpleActivityTest(Acts['Total'],N[1],Nbins)
+        CS.append(stest['pvalue'])
+    C=score_C(np.array(CS))
+    return C
 
